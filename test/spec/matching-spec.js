43bfca98cbfee8541a7b6eb4681f219f847c3bb1
Fergus McDowall <fergusmcdowall@gmail.com>
2014-07-13 10:03:46 +0100

    matcher uses TF index

--- test/spec/matching-spec.js
+++ test/spec/matching-spec.js
@@ -3,23 +3,6 @@ var si = require('../../lib/search-index.js');
 
 describe('generation and operation of matcher', function () {
 
-
-  it('generate matcher', function () {
-    runs(function() {
-      this.matchingMsg = '';
-      var that = this;
-      si.generateMatcher(function(matchingMsg) {
-        that.matchingMsg = matchingMsg;  
-      });
-    });
-    waitsFor(function() {
-      return this.matchingMsg != '';
-    }, 'matchingMsg not to be empty', 100000)
-    runs(function () {
-      expect(this.matchingMsg).toEqual('[success] finished generating matcher with 10439 tokens\n');
-    });
-  });
-
   it('matches stuff', function () {
     runs(function() {
       this.matchingMsg = '';
@@ -33,7 +16,7 @@ describe('generation and operation of matcher', function () {
     }, 'matchingMsg not to be empty', 100000)
     runs(function () {
       console.log(this.matchingMsg);
-      expect(this.matchingMsg).toEqual(['london','long','longer','longrange','longstanding','longtime']);
+      expect(this.matchingMsg).toEqual(['long','london','longer','longrange','longstanding','longtime']);
     });
   });
 
