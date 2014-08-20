b990fb0304c6ddadbc395b381e2983c60605967d
Fergus McDowall <fergusmcdowall@gmail.com>
2014-08-20 15:50:17 +0200

    hacking some replication stuff

--- test/spec/5replication-spec.js
+++ test/spec/5replication-spec.js
@@ -7,6 +7,7 @@ var newindexes = level('newsi', {valueEncoding: 'json'});
 
 
 describe('replication', function () {
+  var data = JSON.parse(fs.readFileSync('test/testdata/justOne.json'));
 
   it('should open a new index', function () {
     runs(function() {
@@ -23,30 +24,41 @@ describe('replication', function () {
       expect(this.openingMsg).toEqual('index opened');
     });
   });
-
-
-  it('should be able to create a snapshot', function () {    
-    runs(function () {
-      this.indexPiped = false;
+/*
+  it('should open a new index', function () {
+    runs(function() {
+      this.openingMsg = '';
       var that = this;
-      si.createSnapShot(function(rs) {
-        var ws = newindexes.createWriteStream();
-        rs.pipe(ws).on('close', function(){
-          console.log('index piped');
-          that.indexPiped = true;
-        });
-      });
+      si.open('si', function(openingMsg) {
+        that.openingMsg = openingMsg;  
+      });  
     });
     waitsFor(function() {
-      return this.indexPiped;
-    }, 'waiting for search results', 60000)
+      return this.openingMsg != '';
+    }, 'message returned from search-index', 100000)
+    runs(function () {
+      expect(this.openingMsg).toEqual('index opened');
+    });
+  });
+*/
+  it('should index one file of test data', function () {
     runs(function() {
-      expect(this.indexPiped).toEqual(true);
+      this.indexingMsg = '';
+      var that = this;
+      si.add(data, 'reuters-000.json', ['places'], function(indexingMsg) {
+        that.indexingMsg = indexingMsg;  
+      });  
+    });
+    waitsFor(function() {
+      return this.indexingMsg != '';
+    }, 'indexingMsg not to be empty (search results returned)', 30000)
+    runs(function () {
+      expect(this.indexingMsg).toEqual('[success] indexed batch: reuters-000.json');
     });
   });
 
 
-  it('should be able to replicate from a snapshot', function () {    
+  it('should be able to create a snapshot', function () {    
     runs(function () {
       this.indexPiped = false;
       var that = this;
