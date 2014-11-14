72abbfbdf73f52ac74927971884779ed07f2727e
Fergie McDowall <fergusmcdowall@gmail.com>
2014-11-14 15:04:17 +0100

    added verification and test for malformed data

--- test/spec/0indexing-spec.js
+++ test/spec/0indexing-spec.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
-var si = require('../../lib/search-index.js')({logLevel:'error'});
+var si = require('../../lib/search-index.js')({logLevel:'info'});
 
 
 describe('indexing and search', function () {
+
 
   var data = JSON.parse(fs.readFileSync('node_modules/reuters-21578-json/data/full/reuters-000.json'));
 
@@ -19,6 +20,25 @@ describe('indexing and search', function () {
     }, 'err not to be empty (search err returned)', 30000)
     runs(function () {
       expect(this.err).toEqual(false);
+    });
+  });
+
+
+  var malformedData = JSON.parse('[1,2,3,4,5]');
+
+  it('should fail prettily with malformed data', function () {
+    runs(function() {
+      this.err = undefined;
+      var that = this;
+      si.add({'batchName': 'malformed batch', 'filters': ['places']}, malformedData, function(err) {
+        that.err = err;
+      });
+    });
+    waitsFor(function() {
+      return this.err != undefined;
+    }, 'err to be true', 30000)
+    runs(function () {
+      expect(this.err).toEqual(true);
     });
   });
 
