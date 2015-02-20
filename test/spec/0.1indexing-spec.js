8e64dfe50c96f19ca707958c421e353ed6e46dda
Fergie McDowall <fergusmcdowall@gmail.com>
2015-02-20 15:43:02 +0100

    if a specified facet field isnt an array then it is treated as an array with one element

--- test/spec/0.1indexing-spec.js
+++ test/spec/0.1indexing-spec.js
@@ -19,6 +19,7 @@ describe('indexing and search', function () {
     delete datum.sector1;
     delete datum.sector2;
     delete datum.theme_namecode;
+//    datum.totalamt = [datum.totalamt];
     return datum;
   };
 
@@ -30,7 +31,7 @@ describe('indexing and search', function () {
       this.done = false;
       var that = this;
       si.add({'batchName': 'world-bank-projects.json',
-              'filters': ['mjtheme']},
+              'filters': ['mjtheme', 'totalamt']},
              _.map(data, processDoc), function(err) {
         that.err = err;
         that.done = true;
@@ -44,30 +45,6 @@ describe('indexing and search', function () {
     });
   });
 
-  it('should be able to create a snapshot', function () {    
-    runs(function () {
-      this.completed = false;
-      this.error = false;
-      var that = this;
-      si.snapShot(function(rs) {
-        rs.pipe(fs.createWriteStream('backup.gz'))
-          .on('close', function() {
-            that.completed = true;
-          })
-          .on('error', function() {
-            that.error = true;
-          });
-      });
-    });
-    waitsFor(function() {
-      return this.completed;
-    }, 'waiting for search results', 60000)
-    runs(function() {
-      expect(this.completed).toEqual(true);
-      expect(this.error).toEqual(false);
-    });
-  });
-
 
   it('should be able to search in indexed data', function () {    
     runs(function () {
@@ -77,7 +54,7 @@ describe('indexing and search', function () {
         'query': {
           '*': ['ethiopia']
         },
-        'facets': ['mjtheme']
+        'facets': ['mjtheme','totalamt']
       }, function(err, searchResults) {
         that.searchResults = searchResults;
       });
@@ -96,6 +73,39 @@ describe('indexing and search', function () {
       expect(this.searchResults.hits[1].id).toEqual('P123531');
       expect(this.searchResults.hits[2].id).toEqual('P117731');
       expect(this.searchResults.hits[3].id).toEqual('P128891');
+    });
+  });
+
+
+  it('faceting for totalamt should work even though input is not in array format', function () {
+    runs(function () {
+      this.searchResults = '';
+      var that = this;
+      si.search({
+        'query': {
+          '*': ['ethiopia']
+        },
+        'facets': ['totalamt']
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
+    });
+    waitsFor(function() {
+      return this.searchResults != '';
+    }, 'waiting for search results', 1000)
+    runs(function() {
+      expect(this.searchResults).toBeDefined();
+      expect(this.searchResults.hits.length).toEqual(4);
+      expect(this.searchResults.facets.totalamt).toBeDefined();
+      expect(this.searchResults.facets.totalamt[0].key).toEqual('100000000');
+      expect(this.searchResults.facets.totalamt[0].value).toEqual(1);
+      expect(this.searchResults.facets.totalamt[1].key).toEqual('130000000');
+      expect(this.searchResults.facets.totalamt[1].value).toEqual(1);
+      expect(this.searchResults.facets.totalamt[2].key).toEqual('415000000');
+      expect(this.searchResults.facets.totalamt[2].value).toEqual(1);
+      expect(this.searchResults.facets.totalamt[3].key).toEqual('600000000');
+      expect(this.searchResults.facets.totalamt[3].value).toEqual(1);
+      expect(this.searchResults.facets.totalamt.length).toEqual(4);
     });
   });
 
