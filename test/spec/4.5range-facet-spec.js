c99b7a85f257d340bc9714d431ccba4f5986f7fe
Fergie McDowall <fergusmcdowall@gmail.com>
2015-03-11 19:18:25 +0100

    improved range faceting

--- test/spec/4.5range-facet-spec.js
+++ test/spec/4.5range-facet-spec.js
@@ -2,7 +2,6 @@ describe('faceting', function () {
 
   var fs = require('fs');
   var si = require('../../')({indexPath:'si-world-bank'});
-
   it('should be able to search and do facet ranges', function () {    
     runs(function () {
       this.searchResults = '';
@@ -38,38 +37,29 @@ describe('faceting', function () {
             ]}
         },
         "offset": 0,
-        "pageSize": 10,
-        "facets": [
-          "totalamt",
-          "mjtheme"
-        ],
-        "facetSort": "keyAsc",
-        "facetLength": 10}, function(err, searchResults) {
-          that.searchResults = searchResults;
-        });
+        "pageSize": 10
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
     });
     waitsFor(function() {
       return this.searchResults != '';
     }, 'waiting for search results', 5000)
     runs(function() {
       expect(this.searchResults).toBeDefined();
-      expect(this.searchResults.facets.totalamt[0].key).toEqual("000000000000000");
-      expect(this.searchResults.facets.totalamt[0].value).toEqual(76);
-      expect(this.searchResults.facets.mjtheme[0].key).toEqual("Economic management");
-      expect(this.searchResults.facets.mjtheme[0].value).toEqual(13);
-      expect(this.searchResults.facetRanges[0].value[0].key).toEqual("000000000000000-000000006000000");
-      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(85);
-      expect(this.searchResults.facetRanges[0].value[1].key).toEqual("000000006000001-010000000000000");
-      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(121);
-      expect(this.searchResults.facetRanges[1].value[0].key).toEqual("A-J");
-      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(135);
-      expect(this.searchResults.facetRanges[1].value[1].key).toEqual("K-Z");
-      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(161);      
+      expect(this.searchResults.facetRanges[0].value[0].key).toEqual("000000006000001-010000000000000");
+      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(121);
+      expect(this.searchResults.facetRanges[0].value[1].key).toEqual("000000000000000-000000006000000");
+      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(85);
+      expect(this.searchResults.facetRanges[1].value[0].key).toEqual("K-Z");
+      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(161);      
+      expect(this.searchResults.facetRanges[1].value[1].key).toEqual("A-J");
+      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(135);
     });
   });
 
 
-/*
+
   it('should be able to search for more than 1 word and show facetranges', function () {    
     runs(function () {
       this.searchResults = '';
@@ -105,36 +95,25 @@ describe('faceting', function () {
               ]}
         },
         "offset": 0,
-        "pageSize": 100,
-        "facets": [
-          "totalamt",
-          "mjtheme"
-        ],
-        "facetSort": "keyAsc",
-        "facetLength": 10}, function(err, searchResults) {
-          that.searchResults = searchResults;
-        });
+        "pageSize": 100
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
     });
     waitsFor(function() {
       return this.searchResults != '';
     }, 'waiting for search results', 5000)
     runs(function() {
-//      console.log(JSON.stringify(this.searchResults, null, 2));
       expect(this.searchResults).toBeDefined();
       expect(this.searchResults.totalHits).toEqual(12);
-      expect(this.searchResults.facets.totalamt[0].key).toEqual("000000000000000");
-      expect(this.searchResults.facets.totalamt[0].value).toEqual(9);
-//TODO: some other of the normal facet values seem wrong here- investigate 
-      expect(this.searchResults.facets.mjtheme[0].key).toEqual("Environment and natural resources management");
-      expect(this.searchResults.facets.mjtheme[0].value).toEqual(5);
-      expect(this.searchResults.facetRanges.totalamt[0].key).toEqual("000000000000000-000000050000000");
-      expect(this.searchResults.facetRanges.totalamt[0].value).toEqual(10);
-      expect(this.searchResults.facetRanges.totalamt[1].key).toEqual("000000050000001-100000000000000");
-      expect(this.searchResults.facetRanges.totalamt[1].value).toEqual(2);
-      expect(this.searchResults.facetRanges.mjtheme[0].key).toEqual("A-J");
-      expect(this.searchResults.facetRanges.mjtheme[0].value).toEqual(8);
-      expect(this.searchResults.facetRanges.mjtheme[1].key).toEqual("K-Z");
-      expect(this.searchResults.facetRanges.mjtheme[1].value).toEqual(9);      
+      expect(this.searchResults.facetRanges[0].value[0].key).toEqual("000000000000000-000000050000000");
+      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(10);
+      expect(this.searchResults.facetRanges[0].value[1].key).toEqual("000000050000001-100000000000000");
+      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(2);
+      expect(this.searchResults.facetRanges[1].value[0].key).toEqual("K-Z");
+      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(9);      
+      expect(this.searchResults.facetRanges[1].value[1].key).toEqual("A-J");
+      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(8);
     });
   });
 
@@ -155,34 +134,27 @@ describe('faceting', function () {
           "mjtheme": {}
         },
         "offset": 0,
-        "pageSize": 100,
-        "facets": [
-          "totalamt",
-          "mjtheme"
-        ],
-        "facetSort": "keyAsc",
-        "facetLength": 10}, function(err, searchResults) {
-          that.searchResults = searchResults;
-        });
+        "pageSize": 100
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
     });
     waitsFor(function() {
       return this.searchResults != '';
     }, 'waiting for search results', 5000)
     runs(function() {
-//      console.log(JSON.stringify(this.searchResults.facetRanges, null, 2));
       expect(this.searchResults).toBeDefined();
       expect(this.searchResults.totalHits).toEqual(12);
-      expect(this.searchResults.facetRanges.totalamt.length).toEqual(4);
-      expect(this.searchResults.facetRanges.totalamt[0].value).toEqual(9);
-      expect(this.searchResults.facetRanges.totalamt[1].value).toEqual(1);
-      expect(this.searchResults.facetRanges.totalamt[2].value).toEqual(1);
-      expect(this.searchResults.facetRanges.totalamt[3].value).toEqual(1);
-      expect(this.searchResults.facetRanges.mjtheme.length).toEqual(8);
-      expect(this.searchResults.facetRanges.mjtheme[0].value).toEqual(4);
-      expect(this.searchResults.facetRanges.mjtheme[1].value).toEqual(5);
+      expect(this.searchResults.facetRanges[0].value.length).toEqual(4);
+      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(9);
+      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[2].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[3].value).toEqual(1);
+      expect(this.searchResults.facetRanges[1].value.length).toEqual(8);
+      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(5);
+      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(4);
     });
   });
-
 
   it('should be able to search for more than 1 word and no ranges (experiment)', function () { 
     runs(function () {
@@ -195,26 +167,61 @@ describe('faceting', function () {
           ]
         },
         "facetRanges": {
-          "totalamt": {"sort":"valueDesc"},
-          "mjtheme": {"sort":"valueAsc"}
+          "totalamt": {},
+          "mjtheme": {}
         },
         "offset": 0,
-        "pageSize": 100,
-        "facets": [
-          "totalamt",
-          "mjtheme"
-        ],
-        "facetSort": "keyAsc",
-        "facetLength": 10}, function(err, searchResults) {
-          that.searchResults = searchResults;
-        });
+        "pageSize": 100
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
     });
     waitsFor(function() {
       return this.searchResults != '';
     }, 'waiting for search results', 5000)
     runs(function() {
-//      console.log(JSON.stringify(this.searchResults.facetRanges, null, 2));
       expect(this.searchResults).toBeDefined();
+      expect(this.searchResults.totalHits).toEqual(12);
+      expect(this.searchResults.facetRanges[0].value.length).toEqual(4);
+      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(9);
+      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[2].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[3].value).toEqual(1);
+      expect(this.searchResults.facetRanges[1].value.length).toEqual(8);
+      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(5);
+      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(4);
+    });
+  });
+
+
+  it('should be able to limit facet length', function () { 
+    runs(function () {
+      this.searchResults = '';
+      var that = this;
+      si.search({
+        "query": {
+          "*": [
+            "africa", "bank"
+          ]
+        },
+        "facetRanges": {
+          "totalamt": {"sort":"valueAsc"},
+          "mjtheme": {"sort":"valueDesc", "limit": 3}
+        },
+        "offset": 0,
+        "pageSize": 100
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
+    });
+    waitsFor(function() {
+      return this.searchResults != '';
+    }, 'waiting for search results', 5000)
+    runs(function() {
+      expect(this.searchResults).toBeDefined();
+      expect(this.searchResults.totalHits).toEqual(12);
+      expect(this.searchResults.facetRanges[0].value.length).toEqual(4);
+      expect(this.searchResults.facetRanges[1].value.length).toEqual(3);
     });
   });
 
@@ -230,8 +237,9 @@ describe('faceting', function () {
           ]
         },
         "facetRanges": {
-          "totalamt": {"sort":"valueDesc"},
+          "totalamt": {"sort":"keyDesc"},
           "mjtheme": {
+            "sort": "keyAsc",
             "ranges": [
               [
                 "A",
@@ -244,25 +252,33 @@ describe('faceting', function () {
             ]}
         },
         "offset": 0,
-        "pageSize": 100,
-        "facets": [
-          "totalamt",
-          "mjtheme"
-        ],
-        "facetSort": "keyAsc",
-        "facetLength": 10}, function(err, searchResults) {
-          that.searchResults = searchResults;
-        });
+        "pageSize": 100
+      }, function(err, searchResults) {
+        that.searchResults = searchResults;
+      });
     });
     waitsFor(function() {
       return this.searchResults != '';
     }, 'waiting for search results', 5000)
     runs(function() {
-//      console.log(JSON.stringify(this.searchResults.facetRanges, null, 2));
       expect(this.searchResults).toBeDefined();
+      expect(this.searchResults.totalHits).toEqual(12);
+      expect(this.searchResults.facetRanges[0].value.length).toEqual(4);
+      expect(this.searchResults.facetRanges[0].value[0].key).toEqual('000000300000000');
+      expect(this.searchResults.facetRanges[0].value[0].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[1].key).toEqual('000000070000000');
+      expect(this.searchResults.facetRanges[0].value[1].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[2].key).toEqual('000000020000000');
+      expect(this.searchResults.facetRanges[0].value[2].value).toEqual(1);
+      expect(this.searchResults.facetRanges[0].value[3].key).toEqual('000000000000000');
+      expect(this.searchResults.facetRanges[0].value[3].value).toEqual(9);
+      expect(this.searchResults.facetRanges[1].value.length).toEqual(2);
+      expect(this.searchResults.facetRanges[1].value[0].key).toEqual('A-J');
+      expect(this.searchResults.facetRanges[1].value[0].value).toEqual(8);
+      expect(this.searchResults.facetRanges[1].value[1].key).toEqual('K-Z');
+      expect(this.searchResults.facetRanges[1].value[1].value).toEqual(9);
     });
   });
 
-*/
 
 })
