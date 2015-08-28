e2d2cf618e44ad85854a304e1e5f893b8ef977e1
mewwts <mats@plysjbyen.net>
2015-08-28 07:48:36 +0200

    Search-index can run in localstorage

---
+++ doc/examples/browserify-localstorage/main.js
@@ -0,0 +1,26 @@
+var localstorage = require('localstorage-down');
+
+
+
+var si = require('../../../')({db: localstorage});
+
+var dataset = require('../../../node_modules/reuters-21578-json/data/justTen/justTen.json');
+
+si.add({'batchName': 'reuters'}, dataset, function(err) {
+  if (!err) console.log('indexed!');
+});
+
+document.getElementById('query').addEventListener('keyup', function() {
+  var query = {
+    "query": {
+      "*": this.value.split(' ')
+    }
+  };
+  si.search(query, function(err, results) {
+    var resultHTML = '<b>total hits: </b>' + results.totalHits+ '<p>';
+    for (var i = 0; i < results.hits.length; i++)
+      resultHTML += '<div>' + JSON.stringify(results.hits[i]) + '</div><hr>';
+    document.getElementById('results').innerHTML = resultHTML;
+  });
+});
+
