abedbb8ac8561a0a3cc54128198077da4d084779
Fergie McDowall <fergusmcdowall@gmail.com>
2016-04-11 15:27:49 +0200

    updated tests to new API

--- test/node/twitter-tests/filter-test.js
+++ test/node/twitter-tests/filter-test.js
@@ -3,122 +3,240 @@
 
 var should = require('should')
 
-describe('Filters: ', function () {
+var si
 
-  var si
-
-  it('should initialize the search index', function (done) {
-    require('../../../')({
-      indexPath: 'test/sandbox/si-twitter',
-      logLevel: 'error'
-    }, function (err, thisSi) {
-      if (err) false.should.eql(true)
-      si = thisSi
-      done()
-    })
-  })
-
-  it('should index twitter data', function (done) {
-    this.timeout(60000)
-    var data = require('./twitter-tweets.json')
-    var opt = {}
-    opt.batchName = 'tweetz'
-    opt.fieldOptions = [
-      {fieldName: 'tags', filter: true},
-      {fieldName: 'user', filter: true}
-    ]
-    si.add(data, opt, function (err) {
-      (err === null).should.be.exactly(true)
-      done()
-    })
-  })
-
-  it('should be able to search in twitter data', function (done) {
-    var q = {}
-    q.query = {'*': ['search']}
-    q.facets = {
-      user: {},
-      tags: {}
-    }
-    q.pageSize = 100
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(8)
-      results.totalHits.should.be.exactly(8)
-      should.exist(results.facets[0])
-      results.facets[0].key.should.be.exactly('user')
-      results.facets[0].value[0].key.should.be.exactly('eklem')
-      results.facets[0].value[0].value.should.be.exactly(8)
-      results.facets[0].value[1].key.should.be.exactly('GoogleforWork')
-      ;(results.hits.length > 1).should.be.exactly(true)
-      results.hits.length.should.be.exactly(8)
-      results.hits[0].id.should.be.exactly('1NsXUW')
-      results.hits[1].id.should.be.exactly('TEWP')
-      results.hits[2].id.should.be.exactly('4bU7P5')
-      done()
-    })
-  })
-
-  it('should be able to filter by user', function (done) {
-    var q = {}
-    q.query = {'*': ['search']}
-    q.facets = {
-      user: {},
-      tags: {}
-    }
-    q.filter = {user: [['GoogleforWork', 'GoogleforWork']]}
-    q.pageSize = 100
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(1)
-      results.totalHits.should.be.exactly(1)
-      should.exist(results.facets[0])
-      results.hits[0].id.should.be.exactly('4EaEkI')
-      done()
-    })
-  })
-
-  it('should be able to filter by tag', function (done) {
-    var q = {}
-    q.query = {'*': ['search']}
-    q.facets = {
-      user: {},
-      tags: {}
-    }
-    q.filter = {tags: [['search', 'search']]}
-    q.pageSize = 100
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(5)
-      results.totalHits.should.be.exactly(5)
-      should.exist(results.facets[0])
-      results.hits[0].id.should.be.exactly('TEWP')
-      results.hits[1].id.should.be.exactly('4bU7P5')
-      results.hits[2].id.should.be.exactly('3swrN')
-      done()
-    })
-  })
-
-  it('should be able to search on tokens that are only found in metadata', function (done) {
-    var q = {}
-    q.query = {'*': ['eklem']}
-    q.facets = {
-      user: {},
-      tags: {}
-    }
-    q.pageSize = 100
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(64)
-      results.totalHits.should.be.exactly(64)
-      should.exist(results.facets[0])
-      results.hits[0].id.should.be.exactly('tLl7B')
-      results.hits[1].id.should.be.exactly('f9Fl9')
-      done()
-    })
+it('should initialize the search index', function (done) {
+  require('../../../')({
+    indexPath: 'test/sandbox/si-twitter',
+    logLevel: 'error'
+  }, function (err, thisSi) {
+    if (err) false.should.eql(true)
+    si = thisSi
+    done()
   })
 })
+
+it('should index twitter data', function (done) {
+  this.timeout(60000)
+  var data = require('./twitter-tweets.json')
+  var opt = {}
+  opt.batchName = 'tweetz'
+  opt.fieldOptions = [
+    {fieldName: 'tags', filter: true},
+    {fieldName: 'user', filter: true}
+  ]
+  si.add(data, opt, function (err) {
+    (err === null).should.be.exactly(true)
+    done()
+  })
+})
+
+it('should be able to search in twitter data', function (done) {
+  var q = {}
+  q.query = {
+    AND: {'*': ['search']}
+  }
+  q.categories = [{
+    name: 'user'
+  }, {
+    name: 'tags'
+  }]
+  q.pageSize = 100
+  si.search(q, function (err, results) {
+    should.exist(results)
+    ;(err === null).should.be.exactly(true)
+    results.hits.length.should.be.exactly(8)
+    results.totalHits.should.be.exactly(8)
+    results.categories.should.eql([ 
+      {
+        "key": "tags",
+        "value": [
+          {
+            "key": "search",
+            "value": 5
+          },
+          {
+            "key": "undefined",
+            "value": 3
+          },
+          {
+            "key": "engine",
+            "value": 1
+          },
+          {
+            "key": "eso",
+            "value": 1
+          },
+          {
+            "key": "events",
+            "value": 1
+          },
+          {
+            "key": "searchable",
+            "value": 1
+          },
+          {
+            "key": "sites",
+            "value": 1
+          }
+        ]
+      },
+      {
+        "key": "user",
+        "value": [
+          {
+            "key": "eklem",
+            "value": 8
+          },
+          {
+            "key": "GoogleforWork",
+            "value": 1
+          }
+        ]
+      }
+    ])
+    results.hits.map(function (item) { return item.id }).should.eql(
+      [
+        "1NsXUW",
+        "TEWP",
+        "4bU7P5",
+        "4EaEkI",
+        "3swrN",
+        "3VKiNd",
+        "3FceLy",
+        "2PHH0R"
+      ]
+    )
+    done()
+  })
+})
+
+it('should be able to filter by user', function (done) {
+  var q = {}
+  q.query = {
+    AND: {'*': ['search']}
+  }
+  q.categories = [{
+    name: 'user'
+  }, {
+    name: 'tags'
+  }]
+  q.pageSize = 100
+  q.filter = [
+    {
+      field: 'user',
+      gte: 'GoogleforWork',
+      lte: 'GoogleforWork'
+    }
+  ]
+  si.search(q, function (err, results) {
+    should.exist(results)
+    ;(err === null).should.be.exactly(true)
+    results.hits.length.should.be.exactly(1)
+    results.totalHits.should.be.exactly(1)
+    results.categories.should.eql([{
+      "key": "user",
+      "value": [
+        {
+          "key": "GoogleforWork",
+          "value": 1
+        }
+      ]
+    }])
+    results.hits.map(function (item) { return item.id }).should.eql(
+      [
+        "4EaEkI"
+      ]
+    )
+    done()
+  })
+})
+
+it('should be able to filter by tag', function (done) {
+  var q = {}
+  q.query = {
+    AND: {'*': ['search']}
+  }
+  q.categories = [{
+    name: 'user'
+  }, {
+    name: 'tags'
+  }]
+  q.pageSize = 100
+  q.filter = [
+    {
+      field: 'tags',
+      gte: 'search',
+      lte: 'search'
+    }
+  ]
+  si.search(q, function (err, results) {
+    should.exist(results)
+    ;(err === null).should.be.exactly(true)
+    results.hits.length.should.be.exactly(5)
+    results.totalHits.should.be.exactly(5)
+    results.categories.should.eql(
+      [ 
+        {
+          "key": "tags",
+          "value": [
+            {
+              "key": "search",
+              "value": 5
+            },
+            {
+              "key": "searchable",
+              "value": 1
+            } 
+          ] 
+        } 
+      ]
+    )
+    results.hits.map(function (item) { return item.id }).should.eql(
+      [ 
+        "TEWP",
+        "4bU7P5",
+        "3swrN",
+        "3VKiNd",
+        "2PHH0R"
+      ]
+    )
+    done()
+  })
+})
+
+
+
+it('should be able to search on tokens that are only found in metadata', function (done) {
+  var q = {}
+  q.query = {
+    AND: {'*': ['eklem']}
+  }
+  q.categories = [{
+    name: 'user'
+  }, {
+    name: 'tags'
+  }]
+  q.pageSize = 100
+  si.search(q, function (err, results) {
+    should.exist(results)
+    ;(err === null).should.be.exactly(true)
+    results.hits.length.should.be.exactly(64)
+    results.totalHits.should.be.exactly(64)
+    results.hits.slice(0, 10).map(function (item) { return item.id }).should.eql(
+      [
+        'tLl7B',
+        'f9Fl9',
+        'dVjBP',
+        'cwHKO',
+        'c7hQ1',
+        'Vd7wZ',
+        'TEWP',
+        'RHidS',
+        'QQjZg',
+        'LlDnO'
+      ])
+    done()
+  })
+})
+
