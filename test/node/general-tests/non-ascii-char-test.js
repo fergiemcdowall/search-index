0c3388d7d479516a031debbdb3afb06e8f09877a
Fergie McDowall <fergusmcdowall@gmail.com>
2016-09-08 19:46:02 +0200

    new test

--- test/node/general-tests/non-ascii-char-test.js
+++ test/node/general-tests/non-ascii-char-test.js
@@ -1,40 +1,50 @@
 /* global it */
 /* global describe */
 
-var should = require('should')
-var sandboxPath = 'test/sandbox'
-var searchindex = require('../../../')
+const should = require('should')
+const sandboxPath = 'test/sandbox'
+const SearchIndex = require('../../../')
+const s = new require('stream').Readable()
+const JSONStream = require('JSONStream')
 var si
 
-describe('Indexing and searching non-ascii characters: ', function () {
-  var data = [
-    {
-      id: 1,
-      names: 'ståle synnøve Kjærsti',
-      test: 'this doc should give hits smør for peaches peaches all of the tokens in the names field'
-    },
-    {
-      id: 2,
-      names: 'Gerät Grünnerløkka',
-      test: 'everything in names doc field smør should be searchable searchable searchable'
-    }]
 
+describe('Indexing and searching non-ascii characters: ', function () {
+  s.push(JSON.stringify({
+    id: 1,
+    names: 'ståle synnøve Kjærsti',
+    test: 'this doc should give hits smør for peaches peaches all of the tokens in the names field'
+  }))
+  s.push(JSON.stringify({
+    id: 2,
+    names: 'Gerät Grünnerløkka',
+    test: 'everything in names doc field smør should be searchable searchable searchable'
+  }))
+  s.push(null)
+  
   it('should initialize the first search index', function (done) {
-    searchindex(
-      {indexPath: sandboxPath + '/si-non-ascii',
-       logLevel: 'error'},
-      function (err, thisSi) {
-        if (err) false.should.eql(true)
-        si = thisSi
-        done()
-      })
+    SearchIndex({
+      indexPath: sandboxPath + '/si-non-ascii',
+      logLevel: 'error'
+    }, function (err, thisSi) {
+      si = thisSi
+      return done()
+    })
   })
 
   it('should index test data', function (done) {
-    si.add(data, {}, function (err) {
-      (err === null).should.be.exactly(true)
-      done()
-    })
+    var i = 0
+    s.pipe(JSONStream.parse())
+      .pipe(si.defaultPipeline())
+      .pipe(si.add())
+      .on('data', function(data) {
+        i++
+      })
+      .on('end', function() {
+        i.should.be.exactly(3)
+        true.should.be.exactly(true)
+        return done()
+      })
   })
 
   it('should be able to search in test data', function (done) {
@@ -42,14 +52,17 @@ describe('Indexing and searching non-ascii characters: ', function () {
     q.query = {
       AND: {'*': ['ståle', 'synnøve', 'kjærsti']}
     }
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(1)
-      results.totalHits.should.be.exactly(1)
-      results.hits[0].id.should.be.exactly('1')
-      done()
-    })
+    si.search(q)
+      .on('data', function(data) {
+        JSON.parse(data).document.should.eql({
+          id: '1',
+          names: 'ståle synnøve Kjærsti',
+          test: 'this doc should give hits smør for peaches peaches all of the tokens in the names field'
+        })
+      })
+      .on('end', function() {
+        return done()
+      })
   })
 
   it('should be able to search in test data', function (done) {
@@ -57,13 +70,16 @@ describe('Indexing and searching non-ascii characters: ', function () {
     q.query = {
       AND: {'*': ['gerät', 'grünnerløkka']}
     }
-    si.search(q, function (err, results) {
-      should.exist(results)
-      ;(err === null).should.be.exactly(true)
-      results.hits.length.should.be.exactly(1)
-      results.totalHits.should.be.exactly(1)
-      results.hits[0].id.should.be.exactly('2')
-      done()
-    })
+    si.search(q)
+      .on('data', function(data) {
+        JSON.parse(data).document.should.eql({
+          id: '2',
+          names: 'Gerät Grünnerløkka',
+          test: 'everything in names doc field smør should be searchable searchable searchable'
+        })
+      })
+      .on('end', function() {
+        return done()
+      })
   })
 })
