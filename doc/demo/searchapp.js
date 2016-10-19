4b1f53dba01e5a4f505ea2c9013216bf3c121fef
Fergie McDowall <fergusmcdowall@gmail.com>
2016-10-19 17:03:04 +0200

    added live browser demo

---
+++ doc/demo/searchapp.js
@@ -0,0 +1,73 @@
+// the variable that will become the search index
+var mySearchIndex
+
+// display search results in a div
+const paintResultDiv = function(result) {
+  console.log('Boom')
+  result = JSON.parse(result)
+  var node = document.createElement('div')
+  node.innerHTML = '<b>' + result.document.title + '</b><br>'
+    + result.document.body + '<br>---'
+  document.getElementById('result').appendChild(node)
+}
+
+const search = function (q) {
+  document.getElementById('result').innerHTML = ''
+  mySearchIndex.search(q).on('data', paintResultDiv)
+}
+
+const initIndex = function (err, index) {
+  mySearchIndex = index
+  search()
+}
+
+// index some data
+const indexData = function (data) {
+  highland([data])
+    .pipe(mySearchIndex.defaultPipeline())
+    .pipe(mySearchIndex.add())
+    .on('data', function(data) {})
+    .on('end', search)
+}
+
+// initialize search-index
+SearchIndex({
+  indexPath: 'si-demo',
+  keySeparator: '~'
+}, initIndex)
+
+// PAGE CONTROLLERS
+
+// Add a doc to the index
+document.getElementById("a").onmousedown = function() {
+  const t = document.getElementById("title").value
+  const b = document.getElementById("body").value
+  if ((t + b).length > 0) indexData({ title: t, body: b })
+}
+
+// show the search panel
+document.getElementById("showSearch").onmousedown = function() {
+  document.getElementById("add").style.display = "none"
+  document.getElementById("search").style.display = "block"
+  document.getElementById("s").focus()
+}
+
+// show the add panel
+document.getElementById("showAdd").onmousedown = function() {
+  document.getElementById("add").style.display = "block"
+  document.getElementById("search").style.display = "none"
+  document.getElementById("title").focus()
+}
+
+// do a search
+document.getElementById("s").onkeyup = function() {
+  search(document.getElementById("s").value)
+}
+
+// do a search
+document.getElementById("sa").onmousedown = function() {
+  search()
+}
+
+// init with title field in focus
+document.getElementById("title").focus()
