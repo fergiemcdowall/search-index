2dda37e6c4e185dad7cba66d55c7b9bff856f759
Fergie McDowall <fergusmcdowall@gmail.com>
2018-12-20 11:07:11 +0100

    pushed in search-index-2

---
+++ README.md
@@ -0,0 +1,109 @@
+# search-index-2
+
+new version of search-index that is totally different from the old version
+
+
+This version will eventually be merged into `search-index`
+
+
+## API
+
+Command     |  Accepts    | Returns    | Writes | Description
+----------- |  ---------- | ---------- | ------ | -----------
+`AGGREGATE` |             |            | no     | Can create aggregations by intersecting an array of BUCKETs with a query
+`AND`       |  properties | ids        | no     | Boolean AND. Return IDs of objects that have prop.A AND prop.B
+`BUCKET`    |  properties | ids        | no     | Get all document ids in this namespace
+`DELETE`    |  ids        | ids        | yes    | Delete a set of documents
+`DICTIONARY`|  field name | words      | no     | Return every word in specified field (or entire index if no field specified)
+`DISTINCT`  |  properties | properties | no     | Gets a list of available properties in a space
+`DOCUMENTS` |  ids        | objects    | no     | Get an object by its ID
+`GET`       |  properties | ids        | no     | Get the IDs of objects with a property in the given range
+`INDEX`     |  -          | index      | both   | Get the underlying [index](https://github.com/fergiemcdowall/fergies-inverted-index/).
+`NOT`       |  ids        | ids        | no     | Get all IDs of objects in set A that are not in set B
+`OR`        |  properties | ids        | no     | Boolean OR. Return IDs of objects that have either prop.A OR prop.b
+`PUT`       |  objects    | ids        | yes    | Add objects to index
+`SEARCH`    |  properties | objects    | no     | Equivalent to AND followed by DOCUMENTS
+
+## Getting started
+
+### Initialise and populate an index
+
+```javascript
+
+// Make a new index, or open an existing one with this name
+// EITHER:
+require('search-index').INIT({ name: 'idx' }) // index now available globally as "idx"
+// idx not immediately available if promise resolution is ignored
+// some time later...
+idx.PUT([ /* my array of objects */ ]).then(doStuff)
+
+// OR:
+require('search-index').OPEN({ name: 'idx' }).then(idx => {
+  idx.PUT([ /* my array of objects */ ]).then(doStuff) // no global, idx must be passed around
+  // idx is always open and available
+})
+
+```
+
+### Search the index
+
+```javascript
+
+// (given objects that contain: { land: <land>, colour: <colour>, population: <number> ... })
+
+const { SEARCH, OR } = idx
+
+// get all objects where land=SCOTLAND and colour=GREEN
+SEARCH('land:SCOTLAND', 'colour:GREEN').then(result)
+// (^result is a result set scored and ranked with tfidf)
+
+// you can look as deeply as you want
+SEARCH('metadata.land.fullname:SCOTLAND', 'metadata.colour:GREEN').then(result)
+
+// or search for terms without specifing any fields
+SEARCH('SCOTLAND', 'GREEN').then(result)
+
+// or combine with boolean expressions (see below)
+SEARCH(
+  OR('SCOTLAND', 'IRELAND'),
+  'GREEN'
+).then(result)
+// (these queries can be as deeply nested as required)
+```
+
+
+### Query the index using boolean expressions (AND, OR, NOT)
+
+```javascript
+
+const { AND, NOT, OR } = idx
+
+// AND returns a set of IDs with matched properties
+AND('land:SCOTLAND', 'colour:GREEN').then(result)
+
+// as above, but returning the whole document
+AND('land:SCOTLAND', 'colour:GREEN').then(idx.DOCUMENTS).then(result)
+
+// either land:SCOTLAND OR land:IRELAND
+OR('land:SCOTLAND', 'land:IRELAND').then(result)
+
+// queries can be embedded within each other
+AND(
+  'land:SCOTLAND',
+  OR('colour:GREEN', 'colour:BLUE')
+).then(result)
+
+// get all object IDs where land=SCOTLAND and colour is NOT GREEN
+NOT(
+  'land:SCOTLAND',                      // everything in this set
+  AND('colour:GREEN', 'colour:RED').    // minus everything in this set
+).then(result)
+
+```
+
+### Aggregation
+
+
+
+
+(See the [tests](https://github.com/fergiemcdowall/search-index-2/tree/master/test) for more examples.)
---
+++ test/DELETE-test.js
@@ -0,0 +1,120 @@
+const test = require('tape')
+const sandbox = 'test/sandbox/'
+const si2 = require('../lib/main.js')
+
+const indexName = sandbox + 'delete-test'
+
+const data = [
+  {
+    _id: 'a',
+    title: 'quite a cool document',
+    body: {
+      text: 'this document is really cool cool cool',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 5000
+  },
+  {
+    _id: 'b',
+    title: 'quite a cool document',
+    body: {
+      text: 'this document is really cool bananas',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 500
+  },
+  {
+    _id: 'c',
+    title: 'something different',
+    body: {
+      text: 'something totally different',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 200
+  }
+]
+
+test('create a search index', t => {
+  t.plan(1)
+  si2.INIT({ name: indexName }).then(t.pass)
+})
+
+test('can add some worldbank data', t => {
+  t.plan(1)
+  global[indexName].PUT(data).then(() => {
+    t.pass('ok')
+  })
+})
+
+test('can DELETE', t => {
+  t.plan(1)
+  global[indexName].DELETE([ 'b' ]).then((res) => t.looseEqual(res, [
+    {
+      'title': {
+        'a': '0.25',
+        'cool': '0.25',
+        'document': '0.25',
+        'quite': '0.25'
+      },
+      'body.text': {
+        'bananas': '0.17',
+        'cool': '0.17',
+        'document': '0.17',
+        'is': '0.17',
+        'really': '0.17',
+        'this': '0.17'
+      },
+      'body.metadata': {
+        'coolness': '0.50',
+        'documentness': '0.50'
+      },
+      'importantNumber': {
+        '500': '500'
+      },
+      '_id': 'b',
+      '!doc': {
+        '_id': 'b',
+        'title': 'quite a cool document',
+        'body': {
+          'text': 'this document is really cool bananas',
+          'metadata': 'coolness documentness'
+        },
+        'importantNumber': 500
+      }
+    }
+  ]))
+})
+
+test('verify DELETE', t => {
+  t.plan(1)
+  const indexStructure = []
+  const expectedIndexStructure = [
+    { 'key': 'body.metadata.coolness:0.50', 'value': ['a', 'c'] },
+    { 'key': 'body.metadata.documentness:0.50', 'value': ['a', 'c'] },
+    { 'key': 'body.text.cool:0.60', 'value': ['a'] },
+    { 'key': 'body.text.different:0.33', 'value': ['c'] },
+    { 'key': 'body.text.document:0.20', 'value': ['a'] },
+    { 'key': 'body.text.is:0.20', 'value': ['a'] },
+    { 'key': 'body.text.really:0.20', 'value': ['a'] },
+    { 'key': 'body.text.something:0.33', 'value': ['c'] },
+    { 'key': 'body.text.this:0.20', 'value': ['a'] },
+    { 'key': 'body.text.totally:0.33', 'value': ['c'] },
+    { 'key': 'importantNumber.200:200', 'value': ['c'] },
+    { 'key': 'importantNumber.5000:5000', 'value': ['a'] },
+    { 'key': 'title.a:0.25', 'value': ['a'] },
+    { 'key': 'title.cool:0.25', 'value': ['a'] },
+    { 'key': 'title.different:0.50', 'value': ['c'] },
+    { 'key': 'title.document:0.25', 'value': ['a'] },
+    { 'key': 'title.quite:0.25', 'value': ['a'] },
+    { 'key': 'title.something:0.50', 'value': ['c'] },
+    { 'key': '￮DOC￮a￮', 'value': { 'title': { 'a': '0.25', 'cool': '0.25', 'document': '0.25', 'quite': '0.25' }, 'body.text': { 'cool': '0.60', 'document': '0.20', 'is': '0.20', 'really': '0.20', 'this': '0.20' }, 'body.metadata': { 'coolness': '0.50', 'documentness': '0.50' }, 'importantNumber': { '5000': 5000 }, '_id': 'a', '!doc': { '_id': 'a', 'title': 'quite a cool document', 'body': { 'text': 'this document is really cool cool cool', 'metadata': 'coolness documentness' }, 'importantNumber': 5000 } } },
+    { 'key': '￮DOC￮c￮', 'value': { 'title': { 'different': '0.50', 'something': '0.50' }, 'body.text': { 'different': '0.33', 'something': '0.33', 'totally': '0.33' }, 'body.metadata': { 'coolness': '0.50', 'documentness': '0.50' }, 'importantNumber': { '200': 200 }, '_id': 'c', '!doc': { '_id': 'c', 'title': 'something different', 'body': { 'text': 'something totally different', 'metadata': 'coolness documentness' }, 'importantNumber': 200 } } },
+    { 'key': '￮FIELD￮body.metadata￮', 'value': true },
+    { 'key': '￮FIELD￮body.text￮', 'value': true },
+    { 'key': '￮FIELD￮importantNumber￮', 'value': true },
+    { 'key': '￮FIELD￮title￮', 'value': true }
+  ]
+  global[indexName].INDEX.STORE.createReadStream()
+    .on('data', d => indexStructure.push(d))
+    .on('end', d => t.looseEquals(indexStructure, expectedIndexStructure))
+})
