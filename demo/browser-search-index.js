d520e678d273504c21efd40578084dd3cdd62f7d
Espen Klem <espen.klem@gmail.com>
2019-02-05 22:28:26 +0100

    Nothing working, just some files needed

---
+++ demo/browser-search-index.js
@@ -0,0 +1,32 @@
+/* global si */
+import si from '../dist/search-index.esm.js'
+
+const data = [
+  {
+    _id: 'a',
+    title: 'quite a cool document',
+    body: {
+      text: 'this document is really cool cool cool',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 5000
+  },
+  {
+    _id: 'b',
+    title: 'quite a cool document',
+    body: {
+      text: 'this document is really cool bananas',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 500
+  },
+  {
+    _id: 'c',
+    title: 'something different',
+    body: {
+      text: 'something totally different',
+      metadata: 'coolness documentness'
+    },
+    importantNumber: 200
+  }
+]
