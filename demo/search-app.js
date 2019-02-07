3463ab062d937cf681616b570e4734dd68ce25c1
Espen Klem <espen.klem@gmail.com>
2019-02-07 20:18:58 +0100

    Working but a little buggy example Not sure if it's my code or search-index

---
+++ demo/search-app.js
@@ -0,0 +1,68 @@
+// "lazy load"- db may not be immediately initialized
+db = si({ name: 'searchIndexInBrowser' })
+
+
+// Functions
+const search = function (q) {
+  emptyElements(['searchResults'])
+  db.SEARCH(q).then(function(results) {
+    //console.log(Array.isArray(results))
+    console.log(JSON.stringify(results[0]))
+    results.forEach(function(result) {
+      console.log(result);
+      populateResultsDiv(result)
+    })
+  })
+}
+
+const indexData = function (data) {
+  emptyElements(['title','body'])
+  db.PUT([data]).then(console.log('Indexed'))
+}
+
+const populateResultsDiv = function(result) {
+  console.log('Boom')
+  console.log(result)
+  //result = JSON.parse(result)
+  var node = document.createElement('div')
+  node.innerHTML = '<b>' + result.obj.title + '</b><br>'
+    + result.obj.body + '<br>---'
+  document.getElementById('searchResults').appendChild(node)
+}
+
+const emptyElements = function (elements) {
+  elements.forEach(function(element) {
+    document.getElementById(element).innerHTML = ''
+    document.getElementById(element).value = ''
+  })
+}
+
+// index data
+document.getElementById("indexContent").onmousedown = function() {
+  const docTitle = document.getElementById("title").value
+  const docBody = document.getElementById("body").value
+  if ((docTitle + docBody).length > 0) indexData({ title: docTitle, body: docBody })
+  console.log({ title: docTitle, body: docBody })
+}
+
+// do a search
+document.getElementById("searchQuery").onkeyup = function() {
+  search(document.getElementById("searchQuery").value)
+  console.log('Search query: ')
+  console.log(document.getElementById("searchQuery").value)
+}
+
+
+
+// First iteration to get anything going 
+/*
+db.PUT(data).then(
+  console.log('Data indexed')
+)
+setTimeout(function(){
+  db.SEARCH('document').then(results => {
+    console.log(results)
+  })
+}, 500)
+*/
+
