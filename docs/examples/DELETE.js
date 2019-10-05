639521d594ec31053612ee34d7ae6fe9659665ce
Fergus McDowall <fergus.mcdowall@usit.uio.no>
2019-10-05 11:50:14 +0200

    examples

---
+++ docs/examples/DELETE.js
@@ -0,0 +1,28 @@
+(async () => {
+
+  const si = require('../../')
+  const db = await si({ name: 'nodeQuickstart' })
+  
+  await db.PUT([
+    {
+      _id: 1,
+      bandName: 'The Beatles',
+      description: 'The nice boys of pop'
+    }, {
+      _id: 'two',
+      bandName: 'The Rolling Stones',
+      description: 'The bad boys of rock'
+    }, {
+      _id: 3,
+      bandName: 'The Who',
+      description: 'Nearly as good as Led Zeppelin'
+    }
+  ])
+
+  await db.DOCUMENTS([{_id: '3'}]).then(console.log)
+  
+  await db.DELETE(['3']).then(console.log)
+
+  await db.DOCUMENTS([{_id: '3'}]).then(console.log)
+  
+})()
