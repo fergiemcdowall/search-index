afbe1efcf987aa195a51869437982946f571d2d6
Fergus McDowall <fergus@eduroam-193-157-204-104.wlan.uio.no>
2020-02-21 14:19:45 +0100

    option for case sensitivity (insensitive by default)

--- src/write.js
+++ src/write.js
@@ -2,6 +2,47 @@ import util from './util.js'
 //import trav from 'traverse'
 import tv from 'term-vector'
 
+const scoreArrayTFIDF = arr => {
+  const v = tv(arr)
+  const mostTokenOccurances = v.reduce((acc, cur) => Math.max(cur.positions.length, acc), 0)
+  return v
+    .map(item => item.term[0] + '#'
+             + (((item.positions.length / mostTokenOccurances)).toFixed(2)))
+}
+
+// traverse object, tokenising all leaves (strings to array) and then
+// scoring them
+// `ops` is a collection of indexing pipeline options
+const createDocumentVector = (obj, ops) => Object.entries(obj).reduce((acc, cur) => {
+  ops = Object.assign({
+    caseSensitive: false
+  }, ops || {})
+  if (cur[0] == '_id') {
+    acc[cur[0]] = cur[1]  // return _id "as is"
+  } else if (Array.isArray(cur[1])) {
+    // split up cur[1] into an array or strings and an array of
+    // other things. Then term-vectorize strings and recursively
+    // process other things.
+    const strings = scoreArrayTFIDF(
+      cur[1].filter(item => typeof item === 'string')
+    )
+    const notStrings = cur[1].filter(
+      item => typeof item != 'string'
+    ).map(traverseObject)
+    acc[cur[0]] = strings.concat(notStrings)
+  }
+  else if (typeof cur[1] === 'object') {
+    acc[cur[0]] = traverseObject(cur[1])
+  }
+  else {
+    let str = cur[1].toString().replace(/[^0-9a-z ]/gi, '')
+    if (!ops.caseSensitive) str = str.toLowerCase()
+    acc[cur[0]] = scoreArrayTFIDF(str.split(' '))
+  }
+  return acc
+}, {})
+
+
 export default function (fii) {
 
   const incrementDocCount = increment => fii.STORE.get(
@@ -12,49 +53,16 @@ export default function (fii) {
     // if not found assume value to be 0
     e => fii.STORE.put('￮DOCUMENT_COUNT￮', increment)
   )
-  
-  const scoreArrayTFIDF = arr => {
-    const v = tv(arr)
-    const mostTokenOccurances = v.reduce((acc, cur) => Math.max(cur.positions.length, acc), 0)
-    return v
-      .map(item => item.term[0] + '#'
-           + (((item.positions.length / mostTokenOccurances)).toFixed(2)))
-  }
 
-  // traverse object, tokenising all leaves (strings to array) and then
-  // scoring them
-  const traverseObject = obj => Object.entries(obj).reduce((acc, cur) => {  
-    if (cur[0] == '_id') {
-      acc[cur[0]] = cur[1]  // return _id "as is"
-    } else if (Array.isArray(cur[1])) {
-      // split up cur[1] into an array or strings and an array of
-      // other things. Then term-vectorize strings and recursively
-      // process other things.
-      const strings = scoreArrayTFIDF(
-        cur[1].filter(item => typeof item === 'string')
-      )
-      const notStrings = cur[1].filter(
-        item => typeof item != 'string'
-      ).map(traverseObject)
-      acc[cur[0]] = strings.concat(notStrings)
-    }
-    else if (typeof cur[1] === 'object') {
-      acc[cur[0]] = traverseObject(cur[1])
-    }
-    else {
-      acc[cur[0]] = scoreArrayTFIDF(
-        cur[1].toString().split(' ')
-      )  
-    }
-    return acc
-  }, {})
-
-  const PUT = docs => fii.PUT(
-    docs.map(traverseObject)).then(documentVector => {
-      // globally set docVec length
-      D = documentVector.length + D
-      return incrementDocCount(documentVector.length).then(() => documentVector)
-    })
+  const PUT = (docs, ops) => fii.PUT(
+    docs.map(doc => createDocumentVector(doc, ops))
+  ).then(documentVector => Promise.all(
+    docs.map(doc =>
+      fii.STORE.put('￮DOC_RAW￮' + doc._id + '￮', doc)
+    )).then(
+      result => incrementDocCount(documentVector.length)
+    )
+  )
 
   return {
     // TODO: surely this can be DELETE: fii.DELETE?
--- test/src/SEARCH-test.js
+++ test/src/SEARCH-test.js
@@ -3,7 +3,7 @@ import test from 'tape'
 
 const sandbox = 'test/sandbox/'
 const indexName = sandbox + 'SEARCH'
-const sanitise = str => str.replace(/[^0-9a-z ]/gi, '').toLowerCase()
+//const sanitise = str => str.replace(/[^0-9a-z ]/gi, '').toLowerCase()
 
 test('create a search index', t => {
   t.plan(1)
@@ -17,43 +17,43 @@ test('can add data', t => {
   const data = [
     {
       "_id": 0,
-      text: sanitise("The Beatles were an English rock band formed in Liverpool in 1960. With a line-up comprising John Lennon, Paul McCartney, George Harrison and Ringo Starr, they are regarded as the most influential band of all time.")
+      text: "The Beatles were an English rock band formed in Liverpool in 1960. With a line-up comprising John Lennon, Paul McCartney, George Harrison and Ringo Starr, they are regarded as the most influential band of all time."
     },
     {
       "_id": 1,
-      text: sanitise("We saw the suits in a window and just went in and got them. We all got one, and suddenly that was a uniform. We were going to all these shops and buying little uniforms for ourselves. That’s also why we looked like Beatles; beside the haircut, we were all looking the same")
+      text: "We saw the suits in a window and just went in and got them. We all got one, and suddenly that was a uniform. We were going to all these shops and buying little uniforms for ourselves. That’s also why we looked like Beatles; beside the haircut, we were all looking the same"
     },
     {
       "_id": 2,
-      text: sanitise("Over 50 years after the The Beatles’ ‘White Album’ first stormed the charts, the Fab Four’s iconic double-album achieved 24-time Platinum-certified status – making it the fourth-highest certified release in US history.")
+      text: "Over 50 years after the The Beatles’ ‘White Album’ first stormed the charts, the Fab Four’s iconic double-album achieved 24-time Platinum-certified status – making it the fourth-highest certified release in US history."
     },
     {
       "_id": 3,
-      text: sanitise("I was relieved to discover the reality is very different to the myth,” continues Jackson, “it’s simply an amazing historical treasure-trove. Sure, there’s moments of drama - but none of the discord this project has long been associated with. Watching John, Paul, George, and Ringo work together, creating now-classic songs from scratch, is not only fascinating - it’s funny, uplifting and surprisingly intimate")
+      text: "I was relieved to discover the reality is very different to the myth,” continues Jackson, “it’s simply an amazing historical treasure-trove. Sure, there’s moments of drama - but none of the discord this project has long been associated with. Watching John, Paul, George, and Ringo work together, creating now-classic songs from scratch, is not only fascinating - it’s funny, uplifting and surprisingly intimate"
     },
     {
       "_id": 4,
-      text: sanitise("Paul McCartney will make his eagerly anticipated return to the road with his new 'Freshen Up' tour.")
+      text: "Paul McCartney will make his eagerly anticipated return to the road with his new 'Freshen Up' tour."
     },
     {
       "_id": 5,
-      text: sanitise("John Lennon's most celebrated solo album will be honoured with a number of special releases on October 5 including the six-disc Imagine – The Ultimate Collection.")
+      text: "John Lennon's most celebrated solo album will be honoured with a number of special releases on October 5 including the six-disc Imagine – The Ultimate Collection."
     },
     {
       "_id": 6,
-      text: sanitise("The George Harrison estate is happy to announce HariSongs, a new label created in partnership with Craft Recordings to celebrate the Indian classical music George loved and believed would “help as a balance towards a peaceful daily life.”  HariSongs launches today with two reissues in honour of both Ravi Shankar’s birthday (b. 7th April, 1920) and Ali Akbar Khan’s birthday (b. 14th April, 1922) this month.")
+      text: "The George Harrison estate is happy to announce HariSongs, a new label created in partnership with Craft Recordings to celebrate the Indian classical music George loved and believed would “help as a balance towards a peaceful daily life.”  HariSongs launches today with two reissues in honour of both Ravi Shankar’s birthday (b. 7th April, 1920) and Ali Akbar Khan’s birthday (b. 14th April, 1922) this month."
     },
     {
       "_id": 7,
-      text: sanitise("The Beatles’ legendary animated hit film YELLOW SUBMARINE is returning to cinemas across the UK, Ireland and the U.S. this summer.")
+      text: "The Beatles’ legendary animated hit film YELLOW SUBMARINE is returning to cinemas across the UK, Ireland and the U.S. this summer."
     },
     {
       "_id": 8,
-      text: sanitise("Paul has confirmed his first live dates of 2018 headlining the Austin City Limits Music Festival. The festival will take place across the weekends of 5-7th and 12-14th October. Other artists set to appear include Metallica, Childish Gambino, Arctic Monkeys, Travis Scott, Odesza, The National and more.")
+      text: "Paul has confirmed his first live dates of 2018 headlining the Austin City Limits Music Festival. The festival will take place across the weekends of 5-7th and 12-14th October. Other artists set to appear include Metallica, Childish Gambino, Arctic Monkeys, Travis Scott, Odesza, The National and more."
     },
     {
       "_id": 9,
-      text: sanitise("Paul invites you on a musical journey to Egypt Station, estimated time of arrival Friday 7th September 7, 2018 by way of Capitol Records.")
+      text: "Paul invites you on a musical journey to Egypt Station, estimated time of arrival Friday 7th September 7, 2018 by way of Capitol Records."
     }
   ]
 
